1a8878cb62f0a96d87d40e9a65cbfc7b223360b4
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-10 01:03:05 -0800

    SHIP IS MOVING IN THE VERTICAL AXIS AS WELL NOW SONNNN

--- Kronos/Assets/Player.cs
+++ Kronos/Assets/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour {
     [Tooltip("In ms^-1")][SerializeField] float xSpeed = 6f;
     [Tooltip("In m")][SerializeField] float xRange = 5f;
 
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 6f;
+    [Tooltip("In m")] [SerializeField] float yRange = 5f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -18,9 +21,16 @@ public class Player : MonoBehaviour {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
 
-        float rawXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float xPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xPos, -xRange, xRange);
+
+        float yPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(yPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
 	}
 }
