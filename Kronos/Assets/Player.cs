65e04e370f212cbb4a732c8005a9a6054167b5e1
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-11 00:35:41 -0800

    bullet aesthetics complete, but gameplay needs A LOT of refining

--- Kronos/Assets/Player.cs
+++ Kronos/Assets/Player.cs
@@ -5,7 +5,7 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In ms^-1")][SerializeField] float speed = 10f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 7f;
     [Tooltip("In m")][SerializeField] float xRange = 5f;
     [Tooltip("In m")] [SerializeField] float yRange = 5f;
 
