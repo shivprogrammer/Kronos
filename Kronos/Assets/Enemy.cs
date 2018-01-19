d24303e07dc60125904edab04dff64ecd2aabee0
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-19 00:23:50 -0800

    game is correctly registering when enemy ship is hit with console log

---
+++ Kronos/Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other) {
+        print("the bullets have hit the enemy " + gameObject.name); 
+    }
+}
