569139ad31af57eecbcc380803bf37dd84666825
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-18 23:39:16 -0800

    explosion occuring upon player death!! :)

--- Kronos/Assets/Scripts/CollisionHandler.cs
+++ Kronos/Assets/Scripts/CollisionHandler.cs
@@ -5,8 +5,12 @@ using UnityEditor.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour {
 
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathfx;
+
     void OnTriggerEnter(Collider other) {
         StartDeathSequence();
+        deathfx.SetActive(true);
     }
 
     private void StartDeathSequence() {
