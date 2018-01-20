e9a3f858a837c109be536a381469a2fb492e754d
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-20 06:07:44 -0800

    added more enemies

--- Kronos/Assets/Enemy.cs
+++ Kronos/Assets/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,8 @@ public class Enemy : MonoBehaviour {
 	}
 
     void OnParticleCollision(GameObject other) {
-        Instantiate(deathFX, transform.position, Quaternion.identity);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
