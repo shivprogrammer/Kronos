33077656c2f4cb7e4b6bc44ea297f8acbd678a6b
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-22 00:33:55 -0800

    have made the guns of the ship now accessible to the PlayerController script

--- Kronos/Assets/Scripts/PlayerController.cs
+++ Kronos/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
     [Tooltip("In ms^-1")][SerializeField] float controlSpeed = 20f;
     [Tooltip("In m")][SerializeField] float xRange = 5f;
     [Tooltip("In m")] [SerializeField] float yRange = 3f;
+    [SerializeField] GameObject[] guns;
 
     [Header("Screen-position Based")]
     [SerializeField] float positionPitchFactor = -5f;
