b50f33f2f31a33c884bd7d86ddc341a9087f5afe
shivprogrammer <shiv.unadkat@gmail.com>
2018-01-21 23:24:04 -0800

    updates to ScoreBoard script

--- Kronos/Assets/ScoreBoard.cs
+++ Kronos/Assets/ScoreBoard.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour {
 
     int score;
+    Text scoreText;
 
 	// Use this for initialization
 	void Start () {
-		
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
 	}
 	
 	// Update is called once per frame
